e3248ea6d64e3d29d93af771771badb968386525
Wyatt Mattas <mattaswm@gmail.com>
2020-11-03 20:29:43 -0600

    changed location

--- senior_project/csv_test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from os import path as path
-
-stock = 'AMD'
-#if path.exists(f'E:\\senior_project\\ema_csv\\{stock}_EMAS.csv') == True:
-df = pd.read_csv(f'E:\\senior_project\\ema_csv\\{stock}_EMAS.csv')
-
-ema5 = df['ema5'][df.index[-1]]
-print(ema5)
