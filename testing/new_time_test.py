704bb50c7110d1eaf4984c650c7ca2958b35a71f
Wyatt Mattas <mattaswm@gmail.com>
2020-11-16 22:52:31 -0600

    small updates

--- testing/new_time_test.py
+++ testing/new_time_test.py
@@ -7,5 +7,8 @@ iso_day = datetime.now()
 #print(pd.Timestamp(year=2018,month=1,day=3,tz='US/Eastern').isoformat())
 #print(pd.Timestamp(iso_day,tz='US/Eastern').isoformat())
 
-df = pd.read_csv('E:\senior_project\CEI_get_barset_data.csv', index_col=0)
-print(df)
+# df = pd.read_csv('E:\senior_project\CEI_get_barset_data.csv', index_col=0)
+# print(df)
+
+ct = datetime.now().strftime('%H:%M:%S')
+print(ct)
