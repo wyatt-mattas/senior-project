cd80409d82f37ffec9aff22d860d8ef793398826
Wyatt Mattas <mattaswm@gmail.com>
2020-12-15 18:33:49 -0600

    back testing

---
+++ testing/back_testing.py
@@ -0,0 +1,100 @@
+import backtrader as bt
+import csv
+import datetime
+import backtrader.feeds as btfeeds
+from strategies import MAcrossover, RSIStrat
+import logging
+from backtrader_plotting import Bokeh
+# from strategies import LongOnly
+# from numba import jit, cuda
+logger = logging.getLogger(__name__)
+cerebro = bt.Cerebro()
+
+cerebro.broker.set_cash(10000.0)
+
+data = btfeeds.GenericCSVData(
+    dataname='C:\\Users\\matta\\Documents\\Stock_Application\\senior-project\\testing\\btcusdt_data.csv',
+
+    fromdate=datetime.datetime(2019, 1, 1),
+    todate=datetime.datetime(2019, 12, 31),
+
+    nullvalue=0.0,
+
+    timeframe=bt.TimeFrame.Minutes,
+    compression=1,
+    dtformat=('%Y-%m-%d %H:%M:%S'),
+    datetime=0,
+    time=-1,
+    high=2,
+    low=3,
+    open=1,
+    close=4,
+    volume=-1,
+    openinterest=-1
+    )
+
+cerebro.adddata(data)
+
+cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe_ratio', timeframe=bt.TimeFrame.Minutes)
+cerebro.addanalyzer(bt.analyzers.SQN, _name='sqn_ratio')
+#cerebro.optstrategy(MAcrossover, pfast=range(3,10), pslow=range(11,19), pslower=range(20,29))  # Add the trading strategy
+#cerebro.addstrategy(MAcrossover, pfast=9, pslow=12, pslower=20)
+cerebro.addstrategy(RSIStrat, pfast=3, pslow=11, rsi=3, rsiLow=25, rsiHigh=73)
+#cerebro.optstrategy(RSIStrat, rsi=range(2,15))
+# Default position size
+cerebro.addsizer(bt.sizers.SizerFix, stake=1)
+
+if __name__ == '__main__':
+    # run()
+    # cerebro.broker.set_cash(100000)
+
+    try:
+        print('Running...')
+        optimized_runs = cerebro.run()
+        strat = optimized_runs[0]
+        print('Sharpe Ratio:', strat.analyzers.sharpe_ratio.get_analysis())
+        print('SQN Ratio:', strat.analyzers.sqn_ratio.get_analysis())
+        PnL = round(cerebro.broker.get_value() - 10000, 2)
+        print(PnL)
+        b = Bokeh(style='bar', plot_mode='single')
+        #cerebro.plot(b)
+        print('Finished!')
+    except Exception as e:
+        print(f'Run Error {str(e)}')
+
+
+    # try:
+    #     print('Running...')
+    #     optimized_runs = cerebro.run(runonce=False, exactbars=-1)
+    #     print('Finished!')
+    #     final_results_list = []
+    #     # Iterate through list of lists
+    #     for run in optimized_runs:
+    #         for strategy in run:
+    #             PnL = round(cerebro.broker.get_value() - 10000, 2)
+    #             sharpe = strategy.analyzers.sharpe_ratio.get_analysis()
+    #             sqn = strategy.analyzers.sqn_ratio.get_analysis()
+    #             final_results_list.append(
+    # 				[
+    # 					strategy.params.pfast,
+    # 					strategy.params.pslow,
+    #                     strategy.params.rsi,
+    #                     strategy.params.rsiLow,
+    #                     strategy.params.rsiHigh,
+    # 					PnL,
+    # 					sharpe['sharperatio'],
+    #                     sqn['sqn'],
+    # 				]
+    # 			)
+
+    #     sort_by_sharpe = sorted(final_results_list, key=lambda x: x[7], reverse=True)
+    #     # Print top 5 results sorted by Sharpe Ratio
+    #     #c=0
+    #     print('Results')
+    #     #print(final_results_list)
+    #     for line in sort_by_sharpe[:5]:
+    #         print(line)
+
+    # except Exception as e:
+    #     print(f'Error {str(e)}')
+    #     print(logger.exception(e))
---
+++ testing/strategies.py
@@ -0,0 +1,193 @@
+import backtrader as bt
+#from numba import jit, cuda
+
+class LongOnly(bt.Sizer):
+    params = (('stake', 1),)
+
+    def _getsizing(self, comminfo, cash, data, isbuy):
+      if isbuy:
+          return self.p.stake
+
+      # Sell situation
+      position = self.broker.getposition(data)
+      if not position.size:
+          return 0  # do not sell if nothing is open
+
+      return self.p.stake
+
+class PrintClose(bt.Strategy):
+
+	def __init__(self):
+		#Keep a reference to the "close" line in the data[0] dataseries
+		self.dataclose = self.datas[0].close
+
+	def log(self, txt, dt=None):
+		dt = dt or self.datas[0].datetime.date(0)
+		print(f'{dt.isoformat()} {txt}') #Print date and close
+
+	def next(self):
+		self.log('Close: ', self.dataclose[0])
+#@jit(target ="cuda")
+class MAcrossover(bt.Strategy):
+	# Moving average parameters
+    params = (('pfast',5),('pslow',12),('pslower',17))
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        #print(f'{dt.isoformat()} {txt}') # Comment this line when running optimization
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+
+		# Order variable will contain ongoing order details/status
+        self.order = None
+
+		# Instantiate moving averages
+        self.slower_ema = bt.indicators.EMA(self.datas[0], period=self.params.pslower)
+        self.slow_ema = bt.indicators.EMA(self.datas[0], period=self.params.pslow)
+        self.fast_ema = bt.indicators.EMA(self.datas[0], period=self.params.pfast)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # An active Buy/Sell order has been submitted/accepted - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, {order.executed.price:.2f}')
+            elif order.issell():
+                self.log(f'SELL EXECUTED, {order.executed.price:.2f}')
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Reset orders
+        self.order = None
+
+    def next(self):
+
+        # Check for open orders
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+            # We are not in the market, look for a signal to OPEN trades
+
+            #If the 20 SMA is above the 50 SMA
+            # if self.fast_ema[0] > self.slow_ema[0] and self.fast_ema[0] > self.slower_ema[0]:
+            #     if self.fast_ema[-1] < self.slow_ema[-1] and self.fast_ema[-1] < self.slower_ema[-1]:
+            try:
+                if self.fast_ema[-1] < self.slow_ema[-1] and self.fast_ema[-1] < self.slower_ema[-1] and self.slow_ema[-1] < self.slower_ema[-1]:
+                    if self.fast_ema[-2] < self.slow_ema[-2] and self.fast_ema[-2] < self.slower_ema[-2] and self.slow_ema[-2] < self.slower_ema[-2]:
+                        if self.fast_ema[-3] < self.slow_ema[-3] and self.fast_ema[-3] < self.slower_ema[-3] and self.slow_ema[-3] < self.slower_ema[-3]:
+                            self.log(f'BUY CREATE {self.dataclose[0]:2f}')
+                            # Keep track of the created order to avoid a 2nd order
+                            self.order = self.buy()
+                #Otherwise if the 20 SMA is below the 50 SMA
+                elif self.fast_ema[-1] > self.slow_ema[-1] and self.fast_ema[-1] > self.slower_ema[-1] and self.slow_ema[-1] > self.slower_ema[-1]:
+                    if self.fast_ema[-2] > self.slow_ema[-2] and self.fast_ema[-2] > self.slower_ema[-2] and self.slow_ema[-2] > self.slower_ema[-2]: # making sure the it is tending to move upwards
+                        if (((self.fast_ema[-1]-self.slow_ema[-1])/self.slow_ema[-1]*100) > .5 and ((self.fast_ema[-1]-self.slow_ema[-1])/self.slower_ema[-1]*100) > 2):
+                            self.log(f'SELL CREATE {self.dataclose[0]:2f}')
+                            # Keep track of the created order to avoid a 2nd order
+                            self.order = self.sell()
+            except Exception as e:
+                print(f'{str(e)}: Fast EMA{self.fast_ema[-1]}')
+        else:
+            # We are already in the market, look for a signal to CLOSE trades
+            if len(self) >= (self.bar_executed + 5):
+                self.log(f'CLOSE CREATE {self.dataclose[0]:2f}')
+                self.order = self.close()
+
+class RSIStrat(bt.Strategy):
+
+    params = (('pfast',3),('pslow',11),('rsi',3),('rsiLow',25),('rsiHigh',73))
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        #print(f'{dt.isoformat()} {txt}') # Comment this line when running optimization
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        #self.bought = False
+        self.in_position = False
+		# Order variable will contain ongoing order details/status
+        self.order = None
+        self.fast_ema = bt.indicators.EMA(self.dataclose, period=self.params.pfast)
+        self.slow_ema = bt.indicators.EMA(self.dataclose, period=self.params.pslow)
+        self.rsi = bt.talib.RSI(self.dataclose, timeperiod=self.params.rsi)
+
+    def notify_order(self, order):
+        #bought = self.bought
+
+        if order.status in [order.Submitted, order.Accepted]:
+            # An active Buy/Sell order has been submitted/accepted - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                #self.log(f'BUY EXECUTED, {order.executed.price:.2f}')
+                self.log('BUY EXECUTED, %.2f' % self.dataclose[0])
+            elif order.issell():
+                #self.log(f'SELL EXECUTED, {order.executed.price:.2f}')
+                self.log('SELL EXECUTED, %.2f' % self.dataclose[0])
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Reset orders
+        self.order = None
+
+    def next(self):
+        if self.order:
+            return
+        #if the symbol is overbought
+        if self.rsi[0] >= self.params.rsiHigh and self.fast_ema[0] > self.slow_ema[0]:
+            #if in position is true, sell
+            if self.in_position:
+                #print("Sell")
+                self.order = self.sell()
+                self.in_position = False
+            else:
+                pass
+        #if the symbol is oversold
+        elif self.rsi[0] <= self.params.rsiLow and self.fast_ema[0] < self.slow_ema[0]:
+            if self.in_position:
+                pass
+            #buy if not in position
+            else:
+                #print("Buy")
+                self.order = self.buy()
+                self.in_position = True
+
+    # def next(self):
+
+    #     # Check for open orders
+    #     if self.order:
+    #         return
+
+    #     # Check if we are in the market
+    #     if not self.position:
+    #         # We are not in the market, look for a signal to OPEN trades
+
+    #         if self.rsi <= 30 and self.fast_ema[0] > self.slow_ema[0]:
+    #             #if self.bought == False:
+    #             self.order = self.buy()
+    #                 #self.bought == True
+    #                 #return self.bought
+    #         elif self.rsi >= 70 and self.fast_ema[0] < self.slow_ema[0]:
+    #             #if self.bought == False:
+    #             self.order = self.sell()
+    #                 #self.bought == False
+    #                 #return self.bought
+    #     else:
+    #         # We are already in the market, look for a signal to CLOSE trades
+    #         if len(self) >= (self.bar_executed + 4):
+    #             self.log(f'CLOSE CREATE {self.dataclose[0]:2f}')
+    #             self.order = self.close()
